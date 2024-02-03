a2599bf126fb4008061deb05f3d81143bbfd9b34
chino0522 <beakjinho0522@gmail.com>
2024-02-02 22:24:13 -0800

    filled out Vehicle class

--- CarPoolLibrary/Models/Vehicle.cs
+++ CarPoolLibrary/Models/Vehicle.cs
@@ -5,5 +5,30 @@ namespace CarPoolLibrary.Models;
 
 public class Vehicle
 {
-    
+    [Key]
+    [Required]
+    public int VehicleId { get; set; }
+
+    public string? Model { get; set; }
+
+    public string? Make { get; set; }
+
+    public string? Year { get; set; }
+
+    public string? NumberOfSeats { get; set; }
+
+    public string? VehicleType { get; set; }
+
+    public int? MemberId { get; set; }
+
+    public DateTime? Created { get; set; }
+
+    public DateTime? Modified { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    [ForeignKey("MemberId")]
+    public Member? Member { get; set; }
 }
