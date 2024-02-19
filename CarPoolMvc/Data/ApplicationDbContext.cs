c842fe2f4b4f49a426f593a360a1122caa049bdd
Tracy Ly <tly35@my.bcit.ca>
2024-02-18 20:44:45 -0800

    Revert "Delete CarPoolMvc/Data/ApplicationDbContext.cs"
    This reverts commit 9d1adad9410bdba771bce2965bb3f96debc30cc6.

---
+++ CarPoolMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using CarPoolLibrary.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPoolMvc.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Member>? Members { get; set; }
+    public DbSet<Vehicle>? Vehicles { get; set; }
+    public DbSet<Manifest>? Manifests { get; set; }
+    public DbSet<Trip>? Trips { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // builder.Entity<Trip>().HasKey(t => new { t.TripId, t.VehicleId });
+        // builder.Entity<Manifest>().HasKey(m => new { m.ManifestId, m.MemberId });
+        builder.Entity<Trip>().HasIndex(e => new { e.TripId, e.VehicleId }).IsUnique();
+        builder.Entity<Manifest>().HasIndex(e => new { e.ManifestId, e.MemberId }).IsUnique();
+
+        base.OnModelCreating(builder);
+        builder.Entity<Member>().ToTable("Member");
+        builder.Entity<Vehicle>().ToTable("Vehicle");
+        builder.Entity<Trip>().ToTable("Trip");
+        builder.Entity<Manifest>().ToTable("Manifest");
+
+        builder.Seed();
+    }
+}
